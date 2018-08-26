8480046f0b0595d1b1c64e0ddb624b1ea5c81e07
Nikola Simovic <simovic.nikola90@gmail.com>
2018-08-26 16:53:34 +0200

    Added Promise for the image loading so we can add borken image placeholder if it fails to load

--- src/LazyImage.js
+++ src/LazyImage.js
@@ -3,10 +3,9 @@ import PropTypes from "prop-types";
 
 const imageDefaultStyle = {
     display: "block",
-    maxWidth: "none",
-    width: "100%",
-    height: "100%",
-    minHeight: "100px",
+    maxWidth: "100%",
+    height: "auto",
+    minHeight: "20px",
     objectFit: "cover",
     transition: "opacity 0.5s",
     opacity: 0
@@ -30,10 +29,27 @@ class LazyImage extends React.Component {
         }
     }
 
-    preloadImage = img => {
+    tryFetchImage = url => {
+        return new Promise((resolve, reject) => {
+            const image = new Image();
+            image.src = url;
+            image.onload = resolve;
+            image.onerror = reject;
+        });
+    };
+
+    loadImage = img => {
         const src = img.getAttribute("data-src");
-        img.src = src;
-        img.style.opacity = 1;
+        this.tryFetchImage(src)
+            .then(() => {
+                img.src = src;
+            })
+            .catch(() => {
+                // img.src = ... here goes link for the broken image placeholder
+                img.src =
+                    "https://thewindowsclub-thewindowsclubco.netdna-ssl.com/wp-content/uploads/2018/06/Broken-image-icon-in-Chrome.gif";
+            })
+            .finally(() => (img.style.opacity = 1));
     };
 
     intersectionConfig = {
@@ -43,7 +59,7 @@ class LazyImage extends React.Component {
     observer = new IntersectionObserver((entries, self) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                this.preloadImage(entry.target);
+                this.loadImage(entry.target);
                 self.disconnect();
             }
         });
@@ -51,14 +67,7 @@ class LazyImage extends React.Component {
 
     render() {
         const { alt, url } = this.props;
-        return (
-            <img
-                data-src={url}
-                alt={alt}
-                ref={this.imageRef}
-                style={imageDefaultStyle}
-            />
-        );
+        return <img data-src={url} alt={alt} ref={this.imageRef} style={imageDefaultStyle} />;
     }
 }
 
